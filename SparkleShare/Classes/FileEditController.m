91e375f9f8807ebf07d562619868d2b2790faba7
Stefan Bethge <12260+kjyv@users.noreply.github.com>
2015-03-04 01:22:08 +0100

    different way of scolling under keyboard

--- SparkleShare/Classes/FileEditController.m
+++ SparkleShare/Classes/FileEditController.m
@@ -32,6 +32,10 @@
     [textEditView setText:[[NSString alloc] initWithData:_file.content encoding:NSUTF8StringEncoding]];
     
     self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+    shifted = false;
 }
 
 -(void)viewDidAppear:(BOOL)animated {
@@ -45,6 +49,31 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation: (UIInterfaceOrientation) interfaceOrientation {
     return YES;
 }
+
+#pragma mark - keyboard movements
+
+- (void)keyboardWillShow:(NSNotification *)notification
+{
+    UIEdgeInsets insets = self.textEditView.contentInset;
+    insets.bottom = [notification.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue].size.height;
+    self.textEditView.contentInset = insets;
+    
+    insets = self.textEditView.scrollIndicatorInsets;
+    insets.bottom = [notification.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue].size.height;
+    self.textEditView.scrollIndicatorInsets = insets;
+}
+
+-(void)keyboardWillHide:(NSNotification *)notification
+{
+    UIEdgeInsets insets = self.textEditView.contentInset;
+    insets.bottom = 0;
+    self.textEditView.contentInset = insets;
+    
+    insets = self.textEditView.scrollIndicatorInsets;
+    insets.bottom = 0;
+    self.textEditView.scrollIndicatorInsets = insets;
+}
+
 
 - (void)setEditing:(BOOL)flag animated:(BOOL)animated
 {
@@ -81,6 +110,9 @@
 }
 
 - (void)textViewDidBeginEditing:(UITextView *)textView {
+    _oldRect = [self.textEditView caretRectForPosition:self.textEditView.selectedTextRange.end];
+    
+    _caretVisibilityTimer = [NSTimer scheduledTimerWithTimeInterval:0.3 target:self selector:@selector(_scrollCaretToVisible) userInfo:nil repeats:YES];
 }
 
 - (void)textViewDidChange:(UITextView *)textView {
@@ -88,6 +120,40 @@
 }
 
 - (void)textViewDidEndEditing:(UITextView *)textView {
+    [_caretVisibilityTimer invalidate];
+    _caretVisibilityTimer = nil;
+}
+
+- (void)_scrollCaretToVisible
+{
+    //This is where the cursor is at.
+    CGRect caretRect = [self.textEditView caretRectForPosition:self.textEditView.selectedTextRange.end];
+    
+    if(CGRectEqualToRect(caretRect, _oldRect))
+        return;
+    
+    _oldRect = caretRect;
+    
+    //This is the visible rect of the textview.
+    CGRect visibleRect = self.textEditView.bounds;
+    visibleRect.size.height -= (self.textEditView.contentInset.top + self.textEditView.contentInset.bottom);
+    visibleRect.origin.y = self.textEditView.contentOffset.y;
+    
+    //We will scroll only if the caret falls outside of the visible rect.
+    if(!CGRectContainsRect(visibleRect, caretRect))
+    {
+        CGPoint newOffset = self.textEditView.contentOffset;
+        
+        newOffset.y = MAX((caretRect.origin.y + caretRect.size.height) - visibleRect.size.height + 5, 0);
+        
+        [self.textEditView setContentOffset:newOffset animated:YES];
+    }
+    [_caretVisibilityTimer invalidate];
+}
+
+-(void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 @end
