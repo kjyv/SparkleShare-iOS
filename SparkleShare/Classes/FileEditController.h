26f92fc6aeeec174fb3ef94d0d0b19a9f1cfd599
Stefan Bethge <stefan.bethge@alcemy.tech>
2025-09-13 04:12:18 +0200

    Render markdown for preview

--- SparkleShare/Classes/FileEditController.h
+++ SparkleShare/Classes/FileEditController.h
@@ -7,19 +7,23 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <WebKit/WebKit.h>
 #import "SSFile.h"
 #import "UIViewController+AutoPlatformNibName.h"
 
-@interface FileEditController : UIViewController <UITextViewDelegate> {
+@interface FileEditController : UIViewController <UITextViewDelegate, WKNavigationDelegate> {
     bool fileChanged;
     CGRect _oldRect;
     NSTimer* _caretVisibilityTimer;
     UITextRange *_selectedRange;
     CGPoint offset;
+    BOOL _isPreviewMode;
 };
 - (id)initWithFile: (SSFile *) file;
 
 @property (weak) SSFile *file;
 @property (weak, nonatomic) IBOutlet UITextView *textEditView;
+@property (strong, nonatomic) WKWebView *webView;
+@property (assign, nonatomic) BOOL isMarkdownFile;
 
 @end
